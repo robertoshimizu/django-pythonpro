ce64b91a592f6f366e0e534f789d24e2b77dc2a7
robertoshimizu <rshimizubr@gmail.com>
2020-05-17 21:34:54 -0300

    Set DATABASE_URL no ambiente local como sqlite3.

--- pypro/settings.py
+++ pypro/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from decouple import config
+from functools import partial
+
+import dj_database_url
+from decouple import config, Csv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,7 +32,6 @@ DEBUG = config('DEBUG', cast=bool)
 
 # Let it accessible from any domain
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
 
 # Application definition
 
@@ -77,11 +79,12 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL', default = default_db_url, cast=parse_database)
 }
 
 
