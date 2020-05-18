fed985e144dd2786b0757d7799905cda32eef426
robertoshimizu <rshimizubr@gmail.com>
2020-05-17 21:04:36 -0300

    Decoupling SECRET KEY

--- pypro/settings.py
+++ pypro/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9y22v!l^sb9mx(*@*wbhu7jn*5$n#6cecd2n2#9m=us07^v_5-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
